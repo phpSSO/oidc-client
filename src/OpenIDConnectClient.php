021830560ade88c18f756f79c7c5080f2d92ec67
JuliusPC <github@mepnet.de>
2021-05-02 21:36:17 +0200

    make ID token validation more standard compliant regarding exp and iat

--- src/OpenIDConnectClient.php
+++ src/OpenIDConnectClient.php
@@ -1032,7 +1032,8 @@ class OpenIDConnectClient
         return (($this->issuerValidator->__invoke($claims->iss))
             && (($claims->aud === $this->clientID) || in_array($this->clientID, $claims->aud, true))
             && ($this->unsafeDisableNonce || $claims->nonce === $this->getNonce())
-            && ( !isset($claims->exp) || ((gettype($claims->exp) === 'integer') && ($claims->exp >= time() - $this->leeway)))
+            && ( isset($claims->exp) && ((gettype($claims->exp) === 'integer') && ($claims->exp >= time() - $this->leeway)))
+            && ( isset($claims->iat) && ((gettype($claims->iat) === 'integer') && ($claims->iat <= time() + $this->leeway)))
             && ( !isset($claims->nbf) || ((gettype($claims->nbf) === 'integer') && ($claims->nbf <= time() + $this->leeway)))
             && ( !isset($claims->at_hash) || $claims->at_hash === $expected_at_hash )
         );
